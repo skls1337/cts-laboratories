3d90ea3436174aec3c8f31568675f8390cbf2325
Andy <andygrecu2000@gmail.com>
2021-05-18 11:29:42 +0300

    unit testing commit

---
+++ Lab12-Unit Testing/src/ro/ase/csie/cts/g1092/testing/main/App.java
@@ -0,0 +1,22 @@
+package ro.ase.csie.cts.g1092.testing.main;
+
+import ro.ase.csie.cts.g1092.testing.models.MathOperations;
+
+public class App {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 5;
+
+		int expectedResult = 10;
+
+		int result = MathOperations.add(a, b);
+		if (result == expectedResult) {
+			System.out.println("We know how to add");
+		} else {
+			System.out.println("Take some math classes");
+		}
+
+	}
+
+}
---
+++ Lab12-Unit Testing/src/ro/ase/csie/cts/g1092/testing/tests/TestCaseMathOperations.java
@@ -0,0 +1,51 @@
+package ro.ase.csie.cts.g1092.testing.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ro.ase.csie.cts.g1092.testing.models.MathOperations;
+
+public class TestCaseMathOperations {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		System.out.println("Hello from TestCase");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		System.out.println("Bye from TestCase");
+	}
+
+	@Before
+	public void blabla() throws Exception {
+		System.out.println("Let's test something");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		System.out.println("Cleanup");
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		int a = 5;
+		int b = 6;
+
+		int expectedResult = 11;
+
+		int result = MathOperations.add(a, b);
+		assertEquals("Testing adding 2 int values", expectedResult, result);
+	}
+
+}
